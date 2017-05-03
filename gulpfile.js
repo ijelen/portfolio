da66ebca73b6b4522b8e50cd2f38865c7c744065
Igor Jelen <igorjelen@gmail.com>
2017-05-03 18:12:33 +0100

    Remove SassPath from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,9 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
-var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
-
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
-      includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
     })
       .on('error', $.sass.logError))
